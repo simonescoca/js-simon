3944063fe95d6ecad0a66cc1586a48715d1fc7c5
simonescoca <simone.scoca@icloud.com>
2023-05-25 17:18:17 +0200

    added JSDocs to functions

--- script.js
+++ script.js
@@ -15,32 +15,50 @@
 
 const countDown = setInterval (quantoMancaADomattina, 1000)
 
-function quantoMancaADomattina () {
-    let ora = adesso()
-    let domaniMattina = domattina()
-    let insomma = quantoManca (domaniMattina, ora)
+// FUNCTIONS /////////////////////////////
 
-    console.log (insomma)
-    return insomma
-}
-
-
-function domattina () {
-    let startBoolean = new Date("May 26, 2023 09:30:00").getTime()
-    startBoolean = Math.floor (( startBoolean ) / 1000)
-
-    return startBoolean
-}
-
-
-function adesso () {
+/**
+ * Checks current timestamp
+ * @returns {number} current timestamp
+ */
+function adessoTimestamp () {
     let currentDateAndTime = new Date().getTime()
     currentDateAndTime = Math.floor (( currentDateAndTime ) / 1000)
 
     return currentDateAndTime
 }
 
-function quantoManca (daDataFutura, adAdesso) {
-    const quantoManca = daDataFutura - adAdesso
-    return quantoManca
+/**
+ * Checks tommorrow morning @ 9.30 timestamp
+ * @returns tommorrow morning @ 9.30 timestamp
+ */
+function domattinaTimestamp () {
+    let startBoolean = new Date("May 26, 2023 09:30:00").getTime()
+    startBoolean = Math.floor (( startBoolean ) / 1000)
+
+    return startBoolean
+}
+
+/**
+ * Calculates in seconds the subtraction between two different timestamps
+ * @param {number} daDataFutura Future date
+ * @param {number} adAdesso Current date
+ * @returns the subtratction in seconds
+ */
+function quantoMancaADataFutura (daDataFutura, adAdesso) {
+    const quantoMancaADataFutura = daDataFutura - adAdesso
+    return quantoMancaADataFutura
+}
+
+/**
+ * Calculates in seconds how much is missing to tomorrow morning @ 9.30
+ * @returns {number} number in seconds
+ */
+function quantoMancaADomattina () {
+    let ora = adessoTimestamp()
+    let domaniMattina = domattinaTimestamp()
+    let insomma = quantoMancaADataFutura (domaniMattina, ora)
+
+    console.log (insomma)
+    return insomma
 }
